2bff9a7ab8eb688b44d67ec40e1766663febefc0
Chakrika-P <74602763+Chakrika-P@users.noreply.github.com>
2021-06-13 12:29:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(390, 400);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+image(video,0,0,640,480);
+
+fill(0, 255, 251)
+stroke(2)
+circle(40,40,80,)
+
+fill(5, 0, 79)
+stroke(2)
+circle(120,40,80,)
+
+fill(0, 255, 251)
+stroke(2)
+circle(200,40,80,)
+
+fill(5, 0, 79)
+stroke(2)
+circle(280,40,80,)
+
+fill(0, 255, 251)
+stroke(2)
+circle(360,40,80,)
+
+fill(5, 0, 79)
+stroke(2)
+circle(440,40,80,)
+
+fill(0, 255, 251)
+stroke(2)
+circle(520,40,80,)
+
+fill(5, 0, 79)
+stroke(2)
+circle(600,40,80,)
+
+fill(0, 255, 251)
+stroke(2)
+circle(40,120,80,)
+
+fill(5, 0, 79)
+stroke(2)
+circle(40,200,80,)
+
+fill(0, 255, 251)
+stroke(2)
+circle(40,280,80,)
+
+fill(5, 0, 79)
+stroke(2)
+circle(40,360,80,)
+
+fill(0, 255, 251)
+stroke(2)
+circle(40,440,80,)
+;
